571120be3187446113c92c0d01ae531fe5d02d93
Andrewkizito <Andrewkizito54@gmail.com>
2023-12-15 09:37:09 +0300

    Added Booking Details Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import BookingDetails from "@/page-components/Home/BookingDetails";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <BookingDetails />
+    </>
+  );
 }
