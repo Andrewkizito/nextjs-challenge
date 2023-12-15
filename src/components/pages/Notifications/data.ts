e2a67fcddc465a4bce7a8fba31ccba6c211fcda2
Andrewkizito <Andrewkizito54@gmail.com>
2023-12-15 12:58:03 +0300

    Added Dummy Notifications

---
+++ src/components/pages/Notifications/data.ts
@@ -0,0 +1,59 @@
+// Types
+import type { Notification } from "@/utils/types";
+
+const notifications: Notification[] = [
+  {
+    title: "Mie's beauty parlour accepted your booking req...",
+    subtext: "For June 20th, 2023",
+    amount: 500,
+    type: "business",
+    avatar: "/eclipse.png",
+    timeElapsed: "5m",
+  },
+  {
+    title: "Deposit sent to Jane's beauty Parlour for medium short braids.",
+    subtext: "For June 20th, 2023",
+    amount: 50,
+    type: "business",
+    timeElapsed: "10m",
+  },
+  {
+    title: "Rebecca Wright  sent you a message.",
+    subtext: "Hey are you available to do these conrows for me.....",
+    type: "personal",
+    avatar: "/user.jpeg",
+    timeElapsed: "20m",
+  },
+  {
+    title: "You have cancelled your Natural glam look appointment.",
+    type: "business",
+    isCharge: {
+      text: "$70 Charged",
+    },
+    timeElapsed: "30m",
+  },
+  {
+    title: "Samantha accepted your booking request",
+    subtext: "For June 20th, 2023",
+    amount: 500,
+    type: "business",
+    avatar: "/user-2.png",
+    timeElapsed: "1h",
+  },
+  {
+    title: "Mie's beauty parlour accepted your booking req...",
+    type: "business",
+    avatar: "/eclipse.png",
+    hasActions: true,
+    timeElapsed: "1h 30m",
+  },
+  {
+    title: "Rebecca Wright  sent you a message.",
+    subtext: "Hey are you available to do these conrows for me.....",
+    type: "personal",
+    avatar: "/user.jpeg",
+    timeElapsed: "1h 45m",
+  },
+];
+
+export default notifications;
