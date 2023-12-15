6fc812c1544dbcf58e160190350d750e422ce859
Andrewkizito <Andrewkizito54@gmail.com>
2023-12-15 08:11:04 +0300

    Added Routes

---
+++ src/utils/routes.ts
@@ -0,0 +1,30 @@
+interface Route {
+  name: string;
+  path: string;
+  icon: string;
+}
+
+const appRoutes: Route[] = [
+  {
+    name: "Home",
+    path: "/",
+    icon: "home",
+  },
+  {
+    name: "Appointments",
+    path: "/appointments",
+    icon: "calendar_month",
+  },
+  {
+    name: "Following",
+    path: "/following",
+    icon: "person_add",
+  },
+  {
+    name: "Notifications",
+    path: "/notifications",
+    icon: "notifications_unread",
+  },
+];
+
+export default appRoutes;
