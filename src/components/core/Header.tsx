f08847f38c4f4ea737cf2d8026e1c4f6ffd86d59
Andrewkizito <Andrewkizito54@gmail.com>
2023-12-15 10:24:30 +0300

    Added Responsiveness

--- src/components/core/Header.tsx
+++ src/components/core/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 // UI Components
 import Container from "@/components/Container";
 import Image from "next/image";
@@ -5,14 +6,17 @@ import Link from "next/link";
 
 // Utils
 import appRoutes from "@/utils/routes";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <div className="h-24 shadow-sm border-b border-gray-300 flex items-center justify-center fixed w-full z-30 bg-white">
+    <div className="h-24 shadow-sm border-b border-gray-300 flex items-center justify-center fixed w-full z-30 bg-white max-md:h-16">
       <Container className="flex items-center justify-between">
         <Image src="/logo.png" alt="logo" width={100} height={100} />
-        <div className="flex items-center gap-14">
-          <div className="flex items-center w-96 border border-gray-300 rounded">
+        <div className="flex items-center gap-14 max-lg:gap-10">
+          <div className="flex items-center w-96 border border-gray-300 rounded max-md:hidden max-lg:w-48">
             <div className="h-12 px-5 flex items-center justify-center">
               <span className="material-symbols-outlined">search</span>
             </div>
@@ -22,12 +26,14 @@ const Header = () => {
               placeholder="search"
             />
           </div>
-          <ul className="flex items-center gap-10">
+          <ul className="flex items-center gap-10 max-md:hidden max-lg:gap-5">
             {appRoutes.map((route) => (
               <li key={route.name}>
                 <Link
                   href={route.path}
-                  className="flex flex-col items-center justify-center gap-0.5 duration-300 text-gray-400 hover:text-primary"
+                  className={`flex flex-col items-center justify-center gap-0.5 duration-300 hover:text-primary ${
+                    pathname === route.path ? "text-primary" : "text-gray-500"
+                  }`}
                 >
                   <span className="material-symbols-outlined text-2xl">
                     {route.icon}
@@ -37,6 +43,7 @@ const Header = () => {
               </li>
             ))}
           </ul>
+          <span className="material-symbols-outlined text-2xl md:hidden">menu</span>
         </div>
       </Container>
     </div>
