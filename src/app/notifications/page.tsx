2a716303c19a90856b46f15ef19e5a6f31a44590
Andrewkizito <Andrewkizito54@gmail.com>
2023-12-15 11:30:20 +0300

    Added Notifications View

---
+++ src/app/notifications/page.tsx
@@ -0,0 +1,10 @@
+// UI Components
+import NotificationsView from "@/page-components/Notifications/NotificationsView";
+
+export default function Notifications() {
+  return (
+    <>
+      <NotificationsView />
+    </>
+  );
+}
