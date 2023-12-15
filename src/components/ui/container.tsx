fa8fbd0a5ca29d3ad20daa9f95429b0100fc44c8
Andrewkizito <Andrewkizito54@gmail.com>
2023-12-15 07:32:48 +0300

    UI - Added Container

---
+++ src/components/ui/container.tsx
@@ -0,0 +1,20 @@
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Container: React.FC<ContainerProps> = (props) => {
+  return (
+    <div className="py-20 w-full">
+      <div
+        className={`max-w-7xl w-full max-md:px-5 px-10 mx-auto ${
+          props.className ?? ""
+        }`}
+      >
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+export default Container;
